178e4f44cda40b76fe65a65d8e8c17aeaef6e3bc
Quinn Tucker <qxzcraft@gmail.com>
2023-11-12 00:36:30 -0500

    Fix game_modes.rs

--- src/game_modes.rs
+++ src/game_modes.rs
@@ -3,8 +3,8 @@ use crate::game_state::GameState;
 // The possible game modes.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum GameMode {
-    SCATTER, // 1
-    CHASE,   // 2
+    SCATTER,
+    CHASE,
 }
 
 impl GameMode {
@@ -18,24 +18,20 @@ impl GameMode {
 }
 
 impl GameState {
-    /********************************* Mode Steps *********************************/
-
     // Helper function to get the number of steps until the mode changes
     pub fn get_mode_steps(&self) -> u8 {
-        // Return the mode steps
         return self.modeSteps;
     }
 
     // Helper function to set the number of steps until the mode changes
-    pub fn set_mode_steps(&self, steps: u8) {
-        // (Write) lock the mode steps
-        self.modeSteps = steps; // Set the mode steps
+    pub fn set_mode_steps(&mut self, steps: u8) {
+        self.modeSteps = steps;
     }
 
     // Helper function to decrement the number of steps until the mode changes
-    pub fn decrement_mode_steps(&self) {
+    pub fn decrement_mode_steps(&mut self) {
         if self.modeSteps != 0 {
-            self.modeSteps -= 1; // Decrease the mode steps
+            self.modeSteps -= 1;
         }
     }
 }
